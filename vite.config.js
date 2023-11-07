0b53d55c4f01bc9640e880b413a31d7d99e9d15a
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2023-11-07 20:32:05 +0900

    deploy fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://github.com/harrisfoes/react-quiz",
+  base: "/react-quiz",
   plugins: [react()],
 });
